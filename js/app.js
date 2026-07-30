050355ad5945d0be934c576f72752cd3db7d37bb
kevinchavezg <kevinchavezg@gmail.com>
2026-07-30 17:30:37 -0600

    feat: :rocket: Se cambia funcion de busqueda

--- js/app.js
+++ js/app.js
@@ -6,16 +6,48 @@ async function obtenerDatos() {
         const contenedor = document.getElementById("catalogo");
                 response.forEach(especie =>{
                     contenedor.innerHTML += `
-                        <article class="card">
-                            <h2>${especie.name}</h2>
-                            <p>${especie.classification}</p>
+                        <article class="card" id="card">
+                            <h2>${especie.name}</h2><br>
+                            <p>Clasificación: ${especie.classification}</p>
+                            <p>Estimación de vida: ${especie.average_lifespan}</p>
+                            <p>Color de piel: ${especie.skin_colors}</p>
+                            <p>Color de cabello: ${especie.hair_colors}</p>
+                            <p>Color de ojos: ${especie.eye_colors}</p>
+                            <p>Lenguaje: ${especie.language}</p>
                         </article>
                     `;
                 })
-            }catch(error){
-                console.log(error)
+            } catch (error) {
+                contenedor.innerHTML = `
+                <h2>Error</h2>
+                <p>${error.message}</p>
+                `;
             }
 }
 
+const input = document.getElementById("textoBuscar");
+const boton = document.getElementById("botonBuscar");
+const articulos = document.querySelectorAll("article");
+boton.addEventListener("click", buscarTarjetas);
+
+function buscarTarjetas() {
+    const texto = input.value.toLowerCase().trim();
+    articulos.forEach(articulo => {
+        const contenido = articulo.textContent.toLowerCase();
+        if (contenido.includes(texto)) {
+            articulo.style.display = "block";
+        } else {
+            articulo.style.display = "none";
+        }
+    });
+}
+
 especies = obtenerDatos()
+
+
+input.addEventListener("keydown", (e)=> {
+    if (e.key === "Enter") {
+        boton.click();
+    }
+});
 
