831143648ef45a5a12fb4e8efbe51dbe63a2a568
kevinchavezg <kevinchavezg@gmail.com>
2026-07-30 04:20:04 -0600

    feat: :rocket: Integracion con API

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-function obtenerDatos(){
-    const datosAPI = fetch("https://swapi.info/api/species");
-    //datosAPI.then((res) => res.json());
-    //datosAPI.then((json) => console.log(json));
-    //datosAPI.catch((error) => console.error(error));
-    datosAPI.then((json) => console.log(json));
+async function obtenerDatos() {
+    try {
+        const datosAPI = await fetch("https://swapi.info/api/species");
+        const response = await datosAPI.json();
+        console.log(response);
+        const contenedor = document.getElementById("catalogo");
+                response.forEach(especie =>{
+                    contenedor.innerHTML += `
+                        <article>
+                            <h2>${especie.name}</h2>
+                            <p>${especie.classification}</p>
+                        </article>
+                    `;
+                })
+            }catch(error){
+                console.log(error)
+            }
 }
+
 
 especies = obtenerDatos()
